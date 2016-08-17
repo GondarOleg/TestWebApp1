3b5ee3ca24e4fa6fee6841bb5d727e28917a1856
GondarOleg <_oleg@ua.fm>
2016-08-17 15:36:50 +0300

    f test commit

---
+++ src/main/java/Run.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Hondar.O.M-ad on 17.08.2016.
+ */
+public class Run {
+
+    public static int alwayaTrue(int a, int b){
+        return a - b + 1;
+    }
+
+}
---
+++ src/test/java/TestClass.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class TestClass {
+
+    @Test
+    public void checkIsTrue(){
+        assertTrue(2 == Run.alwayaTrue(4,2));
+
+    }
+
+}
